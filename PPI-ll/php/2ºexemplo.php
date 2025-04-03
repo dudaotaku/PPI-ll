2dd68e5456dc184debd7cd0e0ded13cad12d0900
dudaotaku <dudaotaku25@gmail.com>
2025-04-02 21:49:50 -0300

    aula.php

---
+++ PPI-ll/php/2ºexemplo.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <?php
+    $vetor = array("IFSC", "UFSC", 55, "IFC");  
+    for ($i = 0; $i < sizeof($vetor); $i++) {  
+        echo "<p> valor do vetor no índice " . ($i+1) . ": $vetor[$i]</p>";  
+    }
+   ?>
+</body>
+</html>
---
+++ PPI-ll/php/aula4.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php 
+    $a = 25;
+    echo "<p>A variavel 'a' tem o valor" . $a ."</p>\n";
+    $b = "Boa noite!";
+    echo "<p>A variavel 'a' tem o valor" . $b . "</p>\n";
+
+    echo "<p> a+b tem o valor" . ($a + $b) ."</p> "
+
+?>
+    
+</body>
+</html>
